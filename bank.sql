95a23f512c28a9eb4785917c7474226fea64730d
Lucas Lima <lucas.lima-95@hotmail.com>
2022-02-01 15:19:49 -0300

    Database remodel.

--- bank.sql
+++ bank.sql
@@ -1,9 +1,12 @@
+DROP DATABASE IF EXISTS Bank;
+
 CREATE DATABASE IF NOT EXISTS Bank;
 
 USE Bank;
 
 CREATE TABLE `Customers` (
-  `cpf` VARCHAR(255) PRIMARY KEY,
+  `customer_id` INT PRIMARY KEY AUTO_INCREMENT,
+  `cpf` VARCHAR(255) NOT NULL,
   `first_name` VARCHAR(255) NOT NULL,
   `middle_name` VARCHAR(255) DEFAULT null,
   `last_name` VARCHAR(255) NOT NULL,
@@ -11,45 +14,114 @@ CREATE TABLE `Customers` (
   `created_at` TIMESTAMP DEFAULT (now())
 );
 
-CREATE TABLE `City` (
-  `city_id` INT PRIMARY KEY,
+CREATE TABLE `Cities` (
+  `city_id` INT PRIMARY KEY AUTO_INCREMENT,
   `city` VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE `Branch` (
-  `branch_id` INT PRIMARY KEY,
+CREATE TABLE `Branches` (
+  `branch_id` INT PRIMARY KEY AUTO_INCREMENT,
   `branch` VARCHAR(255) NOT NULL,
   `city_id` INT
 );
 
-CREATE TABLE `Account` (
-  `account_number` INT PRIMARY KEY,
-  `balance` DECIMAL(5,2) NOT NULL,
-  `type` VARCHAR(255) NOT NULL,
-  `status` VARCHAR(255) NOT NULL,
-  `cpf` VARCHAR(255),
+CREATE TABLE `Accounts` (
+  `account_id` INT PRIMARY KEY AUTO_INCREMENT,
+  `account_number` VARCHAR(255) NOT NULL,
+  `balance` DECIMAL(10,2) NOT NULL,
+  `type_id` INT NOT NULL,
+  `status_id` INT NOT NULL,
+  `customer_id` INT NOT NULL,
   `branch_id` INT NOT NULL,
   `created_at` TIMESTAMP DEFAULT (now())
 );
 
-CREATE TABLE `Transaction` (
-  `id` INT PRIMARY KEY,
-  `type` VARCHAR(255) NOT NULL,
-  `amount` DECIMAL(5,2) NOT NULL,
+CREATE TABLE `Transactions` (
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `type_id` INT NOT NULL,
+  `amount` DECIMAL(10, 2) NOT NULL,
   `date` TIMESTAMP DEFAULT (now()),
-  `account_payer` INT,
-  `account_payee` INT
+  `account_payer` INT NOT NULL,
+  `account_payee` INT NOT NULL
 );
 
-ALTER TABLE `Customers` ADD FOREIGN KEY (`city_id`) REFERENCES `City` (`city_id`);
+CREATE TABLE `Account_Types` (
+  `type_id` INT PRIMARY KEY AUTO_INCREMENT,
+  `type` VARCHAR(255) NOT NULL
+);
 
-ALTER TABLE `Branch` ADD FOREIGN KEY (`city_id`) REFERENCES `City` (`city_id`);
+CREATE TABLE `Account_Status` (
+  `status_id` INT PRIMARY KEY AUTO_INCREMENT,
+  `status` VARCHAR(255) NOT NULL
+);
 
-ALTER TABLE `Account` ADD FOREIGN KEY (`cpf`) REFERENCES `Customers` (`cpf`);
+CREATE TABLE `Transaction_Types` (
+  `type_id` INT PRIMARY KEY AUTO_INCREMENT,
+  `type` VARCHAR(255) NOT NULL
+);
 
-ALTER TABLE `Account` ADD FOREIGN KEY (`branch_id`) REFERENCES `Branch` (`branch_id`);
+ALTER TABLE `Customers` ADD FOREIGN KEY (`city_id`) REFERENCES `Cities` (`city_id`);
 
-ALTER TABLE `Transaction` ADD FOREIGN KEY (`account_payer`) REFERENCES `Account` (`account_number`);
+ALTER TABLE `Branches` ADD FOREIGN KEY (`city_id`) REFERENCES `Cities` (`city_id`);
 
-ALTER TABLE `Transaction` ADD FOREIGN KEY (`account_payee`) REFERENCES `Account` (`account_number`);
+ALTER TABLE `Accounts` ADD FOREIGN KEY (`customer_id`) REFERENCES `Customers` (`customer_id`);
 
+ALTER TABLE `Accounts` ADD FOREIGN KEY (`branch_id`) REFERENCES `Branches` (`branch_id`);
+
+ALTER TABLE `Accounts` ADD FOREIGN KEY (`type_id`) REFERENCES `Account_Types` (`type_id`);
+
+ALTER TABLE `Accounts` ADD FOREIGN KEY (`status_id`) REFERENCES `Account_Status` (`status_id`);
+
+ALTER TABLE `Transactions` ADD FOREIGN KEY (`type_id`) REFERENCES `Transaction_Types` (`type_id`);
+
+ALTER TABLE `Transactions` ADD FOREIGN KEY (`account_payer`) REFERENCES `Accounts` (`account_id`);
+
+ALTER TABLE `Transactions` ADD FOREIGN KEY (`account_payee`) REFERENCES `Accounts` (`account_id`);
+
+INSERT INTO Account_Status (status)
+VALUES
+('Inativo'),
+('Ativo');
+
+INSERT INTO Transaction_Types (type)
+VALUES
+('Transferência'),
+('Depósito');
+
+INSERT INTO Account_Types (type)
+VALUES 
+('Conta corrente'),
+('Conta salário'),
+('Conta poupança'),
+('Conta universitária');
+
+INSERT INTO Cities (city)
+VALUES
+('Curitiba'),
+('Belo Horizonte'),
+('São Paulo'),
+('Rio de Janeiro'),
+('Brasília');
+
+INSERT INTO Customers (cpf, first_name, middle_name, last_name, city_id)
+VALUES
+('273.976.317-46', 'Tício', null, 'Ticiano', 1),
+('551.260.186-55', 'Mévio', 'Meio', 'Meviano', 2),
+('527.856.522-58', 'Fulano', null, 'Silva', 3),
+('712.545.792-49', 'Ciclano', 'de', 'Souza', 3),
+('990.073.979-58', 'Beltrano', 'Belo', 'Oliveira', 5);
+
+
+INSERT INTO Branches (branch, city_id)
+VALUES
+('Agência 01', 1),
+('Agência 02', 2),
+('Agência 03', 3),
+('Agência 04', 4);
+
+INSERT INTO Accounts (account_number, balance, type_id, status_id, customer_id, branch_id)
+VALUES
+('318-82-6264', 1500.00, 1, 1, 1, 1),
+('408-92-3605', 700.00, 4, 1, 2, 3),
+('232-37-2069', 20000.00, 1, 1, 3, 1),
+('224-95-7443', 3400.00, 3, 1, 5, 4);
