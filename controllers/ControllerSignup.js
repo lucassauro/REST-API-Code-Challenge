acea506da30dc6c8d8932f695ef481ef88e29970
Lucas Lima <lucas.lima-95@hotmail.com>
2022-02-04 00:41:12 -0300

    Refactored signup controller and service.

--- controllers/ControllerSignup.js
+++ controllers/ControllerSignup.js
@@ -1,15 +1,24 @@
 const ServiceSignup = require('../services/ServiceSignup');
+const { sign } = require('../services/jwt');
 
 const signup = async (req, res) => {
   const {
-    cpf,
     firstName,
     middleName,
     lastName,
   } = req.body;
+
+  const [, hash] = req.headers.authorization.split(' ');
+
+  const [cpf] = Buffer.from(hash, 'base64').toString().split(':');
+
   const result = await ServiceSignup.signup(cpf, firstName, middleName, lastName, req.password);
-  if (result.error) return res.status(400).json(result.error);
-  return res.status(200).json(result);
+
+  if (result.error || !result) return res.status(400).json(result);
+
+  const token = sign(result); // result tem o seguinte formato { costumerId: 123 }
+
+  return res.status(200).json({ result, token });
 };
 
 module.exports = {
--- services/ServiceSignup.js
+++ services/ServiceSignup.js
@@ -1,62 +1,23 @@
-const { sequelize } = require('../database/models');
-const models = require('../database/models');
-const generateSSN = require('./generateSSN');
+const createAccount = require('./createCustomerAndAccount');
+const isCPFRegistered = require('./isCPFRegistered');
+const generateAccountNumber = require('./generateAccountNumber');
 
-const cpfExists = async (cpf) => {
-  const exists = await models.Customer.findOne({ where: { cpf } });
-  if (exists) return { error: { error: 'alreadyExists', message: 'O CPF informado já existe no banco de dados.' } };
-  return false;
+const errors = {
+  alreadyRegistered: {
+    error: 'CPF informado já está cadastrado',
+  },
 };
 
-const accountExists = async (accountNumber) => {
-  const exists = await models.Account.findOne({ where: { accountNumber } });
-  if (exists) return true;
-  return false;
-};
+const signup = async (cpf, firstName, middleName, lastName, hashedPW) => {
+  const customer = await isCPFRegistered(cpf);
 
-const generateAccountNumber = async () => {
-  const accountNumber = generateSSN();
-  if (await accountExists(accountNumber)) return generateSSN();
-  return accountNumber;
-};
+  if (customer) return errors.alreadyRegistered;
 
-const insert = async (cpf, firstName, middleName, lastName, hashedPassword, accountNumber) => {
-  const t = await sequelize.transaction();
-  try {
-    const customer = await models.Customer.create({
-      cpf,
-      firstName,
-      middleName,
-      lastName,
-    }, {
-      transaction: t,
-    });
-    const id = await customer.dataValues.customerId;
-    await models.Account.create({
-      accountNumber,
-      pwHash: hashedPassword,
-      balance: 0,
-      typeId: 1,
-      statusId: 1,
-      customerId: id,
-      branchId: 1,
-    }, {
-      transaction: t,
-    });
-    await t.commit();
-    return { message: `Customer adicionado e conta criada ${accountNumber}` };
-  } catch (e) {
-    await t.rollback();
-    return { message: 'Deu errado' };
-  }
-};
-
-const signup = async (cpf, firstName, middleName, lastName, hashedPassword) => {
-  const exists = await cpfExists(cpf);
   const accountNumber = await generateAccountNumber();
-  if (exists && exists.error) return exists.error;
-  const r = await insert(cpf, firstName, middleName, lastName, hashedPassword, accountNumber);
-  return r;
+
+  const result = await createAccount(cpf, firstName, middleName, lastName, hashedPW, accountNumber);
+
+  return result;
 };
 
 module.exports = {
